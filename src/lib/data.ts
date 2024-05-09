85845c2c59658067fd7410f5267cb18693edc4c2
Sebastian Grisales <sebasgrisales16@gmail.com>
2024-05-09 15:58:06 -0500

    refactor: Update data fetching functions to include cache control options

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,9 @@
 import { bookFromDefaultMapper, bookFromSearchMapper } from "@/mappers/bookMapper";
 
+// https://nextjs.org/docs/app/building-your-application/caching#routerprefetch
+
 export const getBooks = async (query: string) => {
-  const response = await fetch(`http://openlibrary.org/search.json?${query}`);
+  const response = await fetch(`http://openlibrary.org/search.json?${query}`, { cache: 'no-store' });
   const data = await response.json();
 
   if (!data && !data.docs) {
@@ -12,7 +14,7 @@ export const getBooks = async (query: string) => {
 };
 
 export const getDefaultBooks = async () => {
-  const response = await fetch(`http://openlibrary.org/subjects/fantasy.json`);
+  const response = await fetch(`http://openlibrary.org/subjects/fantasy.json`, { cache: 'force-cache' });
   const data = await response.json();
 
   if (!data && !data.works) {
